009ac38a706fd033c4b85576a178e684516a5512
bernardoraos90 <bernardoraos90@gmail.com>
2025-02-04 20:48:43 -0300

    asddfs234

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAwztkMXlDiwxGycs1Yy2CqOALqXnONuxs",
     authDomain: "photofolio-cadf9.firebaseapp.com",
     projectId: "photofolio-cadf9",
-    storageBucket: "photofolio-cadf9.firebasestorage.app",
+    storageBucket: "photofolio-cadf9.appspot.com",
     messagingSenderId: "102949490418",
     appId: "1:102949490418:web:c6f98ae8f22c746c164a6b"
 };
